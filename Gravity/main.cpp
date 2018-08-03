001d7819010271a3caf4bf0e1010ba639f4e8a88
Serhii Herashchenko <sergsy.gerashenko@gmail.com>
2018-08-03 21:01:06 +0300

    <established core of project>

---
+++ Gravity/main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main() {
+	auto window = std::make_shared<sf::RenderWindow>();
+	auto event = std::make_shared<sf::Event>();
+
+	window->create(sf::VideoMode::getDesktopMode(), "Gravity");
+
+	while (window->isOpen()) {
+		while (window->pollEvent(*event)) {
+			if (event->type == sf::Event::Closed) {
+				window->close();
+			}
+		}
+
+		window->clear(sf::Color::Black);
+
+		window->display();
+	}
+}
